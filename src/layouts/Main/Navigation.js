fab964eac491d120ce20016a792b34f3f9f92b93
larfoley <lar_x87@yahoo.com>
2018-07-04 14:54:49 +0100

    added styling to navigation

--- src/layouts/Main/Navigation.js
+++ src/layouts/Main/Navigation.js
@@ -1,11 +1,33 @@
 import React from 'react'
 import { NavLink } from "react-router-dom"
+import styled from 'styled-components'
+
+const StyledNav = styled.nav`
+  background-color: black;
+  a {
+    color: white;
+    text-decoration: none;
+    display: inline-block;
+    padding: 1em;
+    color: gray;
+    transition: .3s;
+    
+    &:hover {
+      background-color: #2e2e2e;
+      color: white;
+    }
+  }
+`
+
+const activeStyle = {
+  color: 'white'
+}
 
 const Navigation = props => (
-  <nav>
-    <NavLink to="/">Home</NavLink>
-    <NavLink to="/about">About</NavLink>
-  </nav>
+  <StyledNav>
+    <NavLink exact to="/" activeStyle={activeStyle}>Home</NavLink>
+    <NavLink to="/about" activeStyle={activeStyle}>About</NavLink>
+  </StyledNav>
 )
 
 export default Navigation
